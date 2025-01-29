1bd20c90cd5d05af7d4d08f3f293a416ec8db099
AbadDev <abadcastro424@gmail.com>
2025-01-28 21:08:09 -0600

    lc

---
+++ Logic/HackerRank/ListComprehensions.py
@@ -0,0 +1,7 @@
+"""
+List comprehensions provide a concise way to create lists.
+Common applications are to make new lists where each element
+is the result of some operations applied to each member of another sequence or iterable,
+or to create a subsequence of those elements that satisfy a certain condition.
+For example, assume we want to create a list of squares, like:
+"""
